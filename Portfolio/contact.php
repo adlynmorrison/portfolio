d748e91609d9c5a4b904dfe7abb94ec8a3a369ec
Adlyn Morrison <adlynm@gmail.com>
2015-01-22 08:17:54 -0600

    portfolio

---
+++ Portfolio/contact.php
@@ -0,0 +1,9 @@
+<div class="divider">
+	<h2>CONTACT</h2>
+<div>
+<section class="section contact">
+	<header>
+		<h4 class="l-4">Let's Work Together</h4>
+		<a class="email" href="mailto:adlynm@gmailcom">Adlyn M (at) Gmail!</a>
+	</header>
+</section>
---
+++ Portfolio/header.php
@@ -0,0 +1,9 @@
+<header class="header">
+	<nav class="nav">
+	    <ul class="ul">
+	        <li class="li l-1">Adlyn M.</li>
+	        <li class="li l-2">Mobile &amp; Web</li>
+	        <li class="li l-3">Designer + Developer</li>
+	    </ul>
+    <nav>
+</header>
